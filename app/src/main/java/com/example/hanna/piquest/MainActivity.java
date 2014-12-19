5ed8094e6b52d9479b36fc2c8825a88c5b407f17
hannaKE <hnke_henke@hotmail.com>
2014-12-19 14:14:28 +0100

    new main and that's pretty much it

--- app/src/main/java/com/example/hanna/piquest/MainActivity.java
+++ app/src/main/java/com/example/hanna/piquest/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.hanna.piquest;
 
 import android.app.ActionBar;
-import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+
+
+
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    static final int NUM_ITEMS = 2;
+
+    MyAdapter mAdapter;
+    ViewPager mPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        // Watch for button clicks.
+        Button button = (Button)findViewById(R.id.goto_first);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(0);
+            }
+        });
+        button = (Button)findViewById(R.id.goto_last);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(NUM_ITEMS-1);
+            }
+        });
     }
 
 
@@ -52,5 +83,21 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
+    }
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ArrayListFragment.newInstance(position);
+        }
     }
 }
